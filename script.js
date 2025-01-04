94b697ef6919ef69a98d9bd0593f3b09d58352a9
Bradley Aboua <clubrocker13@gmail.com>
2025-01-03 23:12:33 -0500

    playing around with the page colors

---
+++ script.js
@@ -0,0 +1,26 @@
+let userName = "John";
+const age = 25;
+const skills = ["HTML","CSS","JavaScript"];
+console.log(skills.at(0));
+console.log("Hello, World!"); 
+const sum = 10 + 5; 
+const product = 10 * 2; 
+console.log(sum, product); 
+firstName = "Jane";
+lastName = "Doe"; 
+console.log("Hello," , firstName , lastName); 
+console.log(`Hello, ${firstName} ${lastName}`);
+function greet(name){
+    return `Hello, ${name}`;
+}
+const greet1 = function greet(name)
+{
+    return `Hello, ${name}!`;
+} 
+console.log(greet1(firstName));
+
+const greet2 = (name) => `Hello ${name}`; /* Arrow functions do not use the function keyword*/
+
+console.log(greet2("Alex")); 
+
+
